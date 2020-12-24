6fa400c42f90eabab2ca1a71fe5c95f46a7e7991
a2krocks <aakapurv@gmail.com>
2020-12-24 22:19:53 +0530

    update ui

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import webview
 from flask import Flask, render_template, request
 
-server = Flask(__name__, static_folder='./assets', template_folder='./templates')
+
+def app(debug=False):
+    server = Flask(__name__, static_folder='./assets', template_folder='./templates')
+
+    @server.route("/", methods=['GET', 'POST'])
+    def home():
+        return render_template("home.html")
+
+    if debug:
+        server.run()
+    else:
+        window = webview.create_window(
+            title="Library Management",
+            width=1366,
+            height=768,
+            min_size=(500, 400),
+            url=server,
+        )
+        webview.start()
 
 
-@server.route("/", methods = ['GET', 'POST'])
-def home():
-    return render_template("home.html")
-
-
-window = webview.create_window(
-    title="Library Management",
-    width=1366,
-    height=768,
-    url=server,
-)
-webview.start()
+app(debug=False)
