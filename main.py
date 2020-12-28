bf043f38244be3a650bced2eac051466d0ac5b03
a2krocks <aakapurv@gmail.com>
2020-12-28 22:54:10 +0530

    test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import webview
 from flask import Flask, render_template, request
 from console_app import app_console
 
+
 def app(debug=False):
     server = Flask(__name__, static_folder='./assets', template_folder='./templates')
     window = webview.create_window(
@@ -9,23 +10,28 @@ def app(debug=False):
         width=1366,
         height=768,
         min_size=(500, 400),
+        background_color="#292929",
         url=server,
     )
 
+    @server.route("/books/view")
+    def booksView():
+        return render_template("/books/books.html")
+
     @server.route("/", methods=['GET', 'POST'])
     def home():
-        return render_template("home.html")
+        return render_template("/home.html")
 
     @server.route("/console")
     def console():
         app_console()
         return "Close This Window"
 
-
     if debug:
         server.run()
     else:
         webview.start()
+
 
 if __name__ == "__main__":
     app(debug=False)
